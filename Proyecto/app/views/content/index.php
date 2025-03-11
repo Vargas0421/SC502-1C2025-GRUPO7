dd7cc4066a72da2bc2715acf7f41054e96f69e33
Brandon <vargasmoreirabrandon@gmail.com>
2025-03-10 22:37:27 -0600

    Update

--- Proyecto/app/views/content/index.php
+++ Proyecto/app/views/content/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Administracion</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href='../css/estilos.css'>
 </head>
 
 <body>
